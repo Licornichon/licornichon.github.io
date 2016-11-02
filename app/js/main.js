5a4c833120d57070751beba7c06db15adb1219c8
Licornichon <antoine.c.payet@gmail.com>
2016-11-02 20:56:15 +0100

    Smooth Scrolling

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,8 @@
-function smoothy(evt) {
-	evt.preventDefault();
-	evt.stopPropagation();
-	window.scrollTo(0, document.getElementById(evt.target.args.substr(1)).offsetTop);
-}
+$('a[href^="#"]').click(function(){
+	var the_id = $(this).attr("href");
 
-window.onload = function init() {
-	var n = document.getElementById("nav");
-	var links = n.getElementsByClassName("link");
-
-	for (var i = 0; i < links.length; i++) {
-		var target = links[i].getAttribute("href");
-		links[i].addEventListener("click", smoothy, false);
-		links[i].args = target;
-	}
-}
+	$('html, body').animate({
+		scrollTop:$(the_id).offset().top
+	}, 'slow');
+	return false;
+});
