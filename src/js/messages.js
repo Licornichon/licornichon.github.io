1c280ec7aa5f1c6503cf2a18372725c576ca26d5
licornichon <antoine.c.payet@gmail.com>
2018-10-01 00:55:47 +0200

    dynamic messages

--- src/js/messages.js
+++ src/js/messages.js
@@ -1,8 +1,32 @@
-const catchphrases = [
-	'Dark theme. Always.',
-	'If you like unicorns, we can be friends.',
-	'tl;dr: I am not good at design but love functional code.',
-	'I need you to scroll down now.',
-	'Please...',
-	'No more, then.',
-]
+(function tldr() {
+	const catchphrases = [
+		'If you like unicorns, we can be friends.',
+		'Dark theme. Always.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		'You will here me singing, badly.',
+		'Black coffee. Black metal. Black... website ?',
+		'I need you to scroll down now.',
+		'Please...',
+		'No really, you would do me a great favor.',
+		'I\'m out.',
+	];
+	const tldr = document.querySelector('.tldr'),
+		elem = tldr.querySelector('span');
+
+	const _changeCatchphrase = () => {
+		tldr.classList.add('hidden');
+		setTimeout(function() {
+			elem.innerHTML = catchphrases.splice(0,1);
+		},250);
+		setTimeout(function() {
+			tldr.classList.remove('hidden');
+		},500);
+	};
+
+	const intrvl = setInterval(() => {
+		_changeCatchphrase();
+		if (!catchphrases.length) {
+			clearInterval(intrvl);
+		};
+	}, 4000);
+})();
